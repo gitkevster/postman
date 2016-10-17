be8d082f0c0ce322997fadc11c79a30e1c725982
Kevin Tang <kevin.tang@tlconsulting.com.au>
2016-10-17 18:21:50 +1100

    More updates to notifications

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Send a test started message to the Slack regression channel
-curl -X POST --data-urlencode "payload={\"username\": \"test-automation-bot\", \"text\": \"Test #$TRAVIS_JOB_NUMBER *STARTED* for *$TESTFOLDER* in repo - *$TRAVIS_REPO_SLUG*\"}" $SLACK_URL
+curl -X POST --data-urlencode "payload={\"username\": \"test-automation-bot\", \"text\": \"Test #$TRAVIS_JOB_NUMBER *STARTED* for *$TESTFOLDER*\"}" $SLACK_URL
 
 # Run the postman collection with the associated environment for the test
 POSTMAN_COLLECTION=$(find $TESTFOLDER -regex ".*/*postman_collection.json")
@@ -11,8 +11,8 @@ docker run -v $PWD:/etc/newman -t postman/newman_alpine33 -c $POSTMAN_COLLECTION
 # Send a test completion message to the Slack regression channel
 if [ "$?" == "0" ]; then
 	# Build passed
-	curl -X POST --data-urlencode "payload={\"username\":\"test-automation-bot\",\"text\":\"Test #$TRAVIS_JOB_NUMBER *PASSED* for *$TESTFOLDER* in repo - *$TRAVIS_REPO_SLUG*\",\"attachments\":[{\"title\":\"Click here to view the results in Travis CI\",\"title_link\":\"https://travis-ci.org/$TRAVIS_REPO_SLUG/jobs/$TRAVIS_JOB_ID\",\"color\":\"good\"}]}" $SLACK_URL;
+	curl -X POST --data-urlencode "payload={\"username\":\"test-automation-bot\",\"text\":\"Test #$TRAVIS_JOB_NUMBER *PASSED* for *$TESTFOLDER*\",\"attachments\":[{\"title\":\"Click here to view the results in Travis CI\",\"title_link\":\"https://travis-ci.org/$TRAVIS_REPO_SLUG/jobs/$TRAVIS_JOB_ID\",\"color\":\"good\"}]}" $SLACK_URL;
 else
 	# Build failed
-	curl -X POST --data-urlencode "payload={\"username\":\"test-automation-bot\",\"text\":\"Test #$TRAVIS_JOB_NUMBER *FAILED* for *$TESTFOLDER* in repo - *$TRAVIS_REPO_SLUG*\",\"attachments\":[{\"title\":\"Click here to view the results in Travis CI\",\"title_link\":\"https://travis-ci.org/$TRAVIS_REPO_SLUG/jobs/$TRAVIS_JOB_ID\",\"color\":\"danger\"}]}" $SLACK_URL;
+	curl -X POST --data-urlencode "payload={\"username\":\"test-automation-bot\",\"text\":\"Test #$TRAVIS_JOB_NUMBER *FAILED* for *$TESTFOLDER*\",\"attachments\":[{\"title\":\"Click here to view the results in Travis CI\",\"title_link\":\"https://travis-ci.org/$TRAVIS_REPO_SLUG/jobs/$TRAVIS_JOB_ID\",\"color\":\"danger\"}]}" $SLACK_URL;
 fi
