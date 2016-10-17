d2a744c2ab9f36cda4f24be4f195f1d6ea5d6a30
Kevin Tang <kevin.tang@tlconsulting.com.au>
2016-10-17 17:50:00 +1100

    Test running parallel tests

--- run-tests.sh
+++ run-tests.sh
@@ -3,14 +3,16 @@
 # Send a test started message to the Slack regression channel
 curl -X POST --data-urlencode "payload={\"username\": \"automation-bot\", \"text\": \"Test #$TRAVIS_BUILD_NUMBER *STARTED* for repo: *$TRAVIS_REPO_SLUG*\"}" $SLACK_URL
 
-# Run the postman collection
-docker run -v $PWD/test/data:/etc/newman -t postman/newman_alpine33 -c HTTPBinNewmanTest.json.postman_collection -e HTTPBinNewmanTestEnv.json.postman_environment
+# Run the postman collection with the associated environment for the test
+POSTMAN_COLLECTION=$(find $TESTFOLDER -regex ".*/*postman_collection.json")
+POSTMAN_ENVIRONMENT=$(find $TESTFOLDER -regex ".*/*postman_environment.json")
+docker run -v $PWD:/etc/newman -t postman/newman_alpine33 -c $POSTMAN_COLLECTION -e $POSTMAN_ENVIRONMENT
 
 # Send a test completion message to the Slack regression channel
 if [ "$?" == "0" ]; then
 	# Build passed
-	curl -X POST --data-urlencode "payload={\"username\":\"automation-bot\",\"text\":\"Test #$TRAVIS_BUILD_NUMBER *PASSED* for repo - *$TRAVIS_REPO_SLUG*\",\"attachments\":[{\"title\":\":Click here to view the results in Travis CI\",\"title_link\":\"https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID\",\"color\":\"good\",\"image_url\":\"$PASSED_ICON\"}]}" $SLACK_URL;
+	curl -X POST --data-urlencode "payload={\"username\":\"automation-bot\",\"text\":\"Test #$TRAVIS_BUILD_NUMBER *PASSED* for repo - *$TRAVIS_REPO_SLUG*\",\"attachments\":[{\"title\":\":Click here to view the results in Travis CI\",\"title_link\":\"https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID\",\"color\":\"good\"}]}" $SLACK_URL;
 else
 	# Build failed
-	curl -X POST --data-urlencode "payload={\"username\":\"automation-bot\",\"text\":\"Test #$TRAVIS_BUILD_NUMBER *FAILED* for repo - *$TRAVIS_REPO_SLUG*\",\"attachments\":[{\"title\":\"Click here to view the results in Travis CI\",\"title_link\":\"https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID\",\"color\":\"danger\",\"image_url\":\"$FAILED_ICON\"}]}" $SLACK_URL;
+	curl -X POST --data-urlencode "payload={\"username\":\"automation-bot\",\"text\":\"Test #$TRAVIS_BUILD_NUMBER *FAILED* for repo - *$TRAVIS_REPO_SLUG*\",\"attachments\":[{\"title\":\"Click here to view the results in Travis CI\",\"title_link\":\"https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID\",\"color\":\"danger\"}]}" $SLACK_URL;
 fi
